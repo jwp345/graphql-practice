1a9157db490ff70049891e9f292dc2f78846a60e
jwp345 <jwp135@naver.com>
2023-09-27 00:53:25 +0900

    Initial commit

---
+++ src/main/java/com/course/graphql/component/fake/FakeHelloDataResolver.java
@@ -0,0 +1,25 @@
+package com.course.graphql.component.fake;
+
+import com.course.graphql.datasource.fake.FakeHelloDataSource;
+import com.netflix.dgs.codegen.generated.types.Hello;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@DgsComponent
+public class FakeHelloDataResolver {
+
+    @DgsQuery
+    List<Hello> allHellos() {
+        return FakeHelloDataSource.HELLO_LIST;
+    }
+
+    @DgsQuery
+    public Hello oneHello() {
+        return FakeHelloDataSource.HELLO_LIST.get(
+                ThreadLocalRandom.current().nextInt(FakeHelloDataSource.HELLO_LIST.size())
+        );
+    }
+}
